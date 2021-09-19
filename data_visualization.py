18a454df771bf7658b5261817273022847ebeb59
Deniz Jasarbasic <Denizj2828@gmail.com>
2021-09-18 23:11:48 -0400

    added visualization of confusion matrix

--- data_visualization.py
+++ data_visualization.py
@@ -1,19 +1,32 @@
 import streamlit as st
 import time
 import numpy as np
+import pandas as pd
 import rsi_converted as dt
 
+# Web app title:
 st.write("""
 # Simple Stock Predictor App
 """)
 
+# Description:
 st.markdown("Language: Python")
-st.markdown("Technologies: :")
+st.markdown("Technologies:")
 st.markdown("Method & Project Description: Our stock prediction model is using a technical analysis approach at predicting the daily price movement of a stock and uses this data to attempt to predict future price movements.")
 st.markdown("Demo:")
 
 
-# Streamlit widgets automatically run the script from top to bottom. Since
-# this button is not connected to any other logic, it just causes a plain
-# rerun.
+
+
+
+st.markdown("Confusion Matrix Visualization:")
+
+# Visualization of the Confusion Matrix:
+df = pd.DataFrame(
+    dt.prediction,
+    columns=('col %d' % i for i in range(500)))
+
+st.dataframe(df)
+
+# Reruns the web app:
 st.button("Re-run")
