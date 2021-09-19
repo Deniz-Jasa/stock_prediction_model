660d68413c0ca8875b856b3c899bccb250fc1847
Deniz Jasarbasic <Denizj2828@gmail.com>
2021-09-18 22:17:45 -0400

    added streamlit demo web app

---
+++ data_visualization.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import time
+import numpy as np
+import rsi_converted as dt
+
+st.write("""
+# Simple Stock Predictor App
+""")
+
+st.markdown("Method & Project Description: Our stock prediction model is using a technical analysis approach at predicting the daily price movement of a stock and uses this data to attempt to predict future price movements.")
+
+st.markdown("Demo:")
+
+
+# Streamlit widgets automatically run the script from top to bottom. Since
+# this button is not connected to any other logic, it just causes a plain
+# rerun.
+st.button("Re-run")
