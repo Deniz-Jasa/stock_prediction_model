db9369f091bdb5f2e11dec7a131628bf9bb8e058
Deniz Jasarbasic <Denizj2828@gmail.com>
2021-09-18 22:19:15 -0400

    added streamlit demo web app

--- data_visualization.py
+++ data_visualization.py
@@ -7,8 +7,9 @@ st.write("""
 # Simple Stock Predictor App
 """)
 
+st.markdown("Language: Python")
+st.markdown("Technologies: :")
 st.markdown("Method & Project Description: Our stock prediction model is using a technical analysis approach at predicting the daily price movement of a stock and uses this data to attempt to predict future price movements.")
-
 st.markdown("Demo:")
 
 
